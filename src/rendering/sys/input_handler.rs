d81d746e7e3a3d7773decc562ee1462a45ff1f95
winterscode <qgv3981g@duck.com>
2024-02-24 19:47:37 -0800

    Commenting the input handler file

--- src/rendering/sys/input_handler.rs
+++ src/rendering/sys/input_handler.rs
@@ -1,18 +1,28 @@
 
+// Load the GLFW crate
 extern crate glfw;
 
+// Get all the requirements for the file
 use glfw::{Action, Context, Key};
 use crate::rendering::sys::dstore::inputs::{Inputs, InputState};
 
+// Create the InputHandler struct with a core lifetime
 pub struct InputHandler<'core> {
     window: &'core mut glfw::PWindow,
     events: &'core glfw::GlfwReceiver<(f64, glfw::WindowEvent)>,
     inputs: Inputs
 }
 
+// Load all the functions for the InputHand;er
 impl <'core> InputHandler<'core>  {
+
+    // Create a new InputHandler
     pub fn new(window: &'core mut glfw::PWindow, events: &'core glfw::GlfwReceiver<(f64, glfw::WindowEvent)>) -> InputHandler<'core> {
+
+        // The struct to save all the inputs
         let mut inputs: Inputs = Inputs::new();
+
+        // Create & pop the input handler
         InputHandler {
             window: window,
             events: events,
@@ -20,8 +30,13 @@ impl <'core> InputHandler<'core>  {
         }
     }
 
+    // Parse every individual input event
     fn handle_individual_event(&mut self, e: glfw::WindowEvent) {
+
+        // Check every possibility
         match e {
+
+            // Pressing the escape key
             glfw::WindowEvent::Key(Key::Escape, _, Action::Press, _) => {
                 self.window.set_should_close(true)
             },
@@ -29,6 +44,7 @@ impl <'core> InputHandler<'core>  {
         }
     }
 
+    // Handle every single event
     pub fn handle_events(&self) {
 
     }
